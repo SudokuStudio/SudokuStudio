5626eb44e7888c7dc6d882348def6e3000dab1ed
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-06 00:20:09 -0700

    Make edit menu accordians work

--- src/svelte/edit/EditSection.svelte
+++ src/svelte/edit/EditSection.svelte
@@ -1,23 +1,51 @@
 <script lang="ts">
     export let title: string;
 
-    import Icon from "../Icon.svelte";
+    export let closed: boolean = false;
+
+    function onClick() {
+        closed = !closed;
+    }
 </script>
 
-<div>
-    <h4 class="section-title">
-        <Icon icon="tree-menu-shown" />
+<div class:closed={closed}>
+    <button class="section-title" on:click={onClick}>
+        <span class="tree-menu icon icon-inline icon-c-clickable icon-tree-menu" />
         {title}
-    </h4>
-    <slot></slot>
+    </button>
+    <div class="panel">
+        <slot></slot>
+    </div>
 </div>
 
 <style lang="scss">
     @use 'src/css/vars';
+
     .section-title {
         margin: 0.5em 0;
         font-weight: vars.$font-weight-heavy;
 
+        font-size: 1em;
+        font-family: inherit;
+        border: 0;
+        padding: 0;
+        background: none;
+        cursor: pointer;
+
         white-space: nowrap;
+    }
+
+    .tree-menu {
+        transition: transform 0.1s ease-in-out;
+    }
+    .closed .tree-menu {
+        transform: rotate(-90deg);
+    }
+
+    // .panel {
+    // }
+    .closed .panel {
+        overflow: hidden;
+        height: 0;
     }
 </style>
