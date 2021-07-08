02fb3f4cc371bf2f1166e0d5ba700879f398ba88
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-07 23:17:57 -0700

    Add GivenRender

--- src/js/board.ts
+++ src/js/board.ts
@@ -1,4 +1,5 @@
 import { StateManager } from './state_manager';
+import { GRID_SCALE } from './consts';
 
 import Diagonal from '../svelte/edit/constraint/Diagonal.svelte';
 import Knight from '../svelte/edit/constraint/Knight.svelte';
@@ -11,6 +12,7 @@ import Arrow from '../svelte/edit/constraint/Arrow.svelte';
 import Sandwich from '../svelte/edit/constraint/Sandwich.svelte';
 
 import BoxRender from '../svelte/board/constraint/BoxRender.svelte';
+import GivenRender from '../svelte/board/constraint/GivenRender.svelte';
 
 export type ConstraintDataAndComponent = {
     id: string,
@@ -30,49 +32,60 @@ export enum ConstraintMenuType {
 export const CONSTRAINT_MENU_TYPES = {
     ['box']: ConstraintMenuType.HIDDEN,
 
+    ['given']: ConstraintMenuType.LOCAL,
+    ['thermo']: ConstraintMenuType.LOCAL,
+    ['arrow']: ConstraintMenuType.LOCAL,
+    ['sandwich']: ConstraintMenuType.LOCAL,
+
     ['diagonal']: ConstraintMenuType.GLOBAL,
     ['knight']: ConstraintMenuType.GLOBAL,
     ['king']: ConstraintMenuType.GLOBAL,
     ['disjointGroups']: ConstraintMenuType.GLOBAL,
     ['consecutive']: ConstraintMenuType.GLOBAL,
-
-    ['given']: ConstraintMenuType.LOCAL,
-    ['thermo']: ConstraintMenuType.LOCAL,
-    ['arrow']: ConstraintMenuType.LOCAL,
-    ['sandwich']: ConstraintMenuType.LOCAL,
 } as const;
 
 export type ConstraintComponent = NonNullable<typeof CONSTRAINT_COMPONENTS[keyof typeof CONSTRAINT_COMPONENTS]>;
 export const CONSTRAINT_COMPONENTS = {
     ['box']: null,
 
+    ['given']: Given,
+    ['thermo']: Thermo,
+    ['arrow']: Arrow,
+    ['sandwich']: Sandwich,
+
     ['diagonal']: Diagonal,
     ['knight']: Knight,
     ['king']: King,
     ['disjointGroups']: DisjointGroups,
     ['consecutive']: Nonconsecutive,
-
-    ['given']: Given,
-    ['thermo']: Thermo,
-    ['arrow']: Arrow,
-    ['sandwich']: Sandwich,
 } as const;
 
 export type ConstraintRenderer = NonNullable<typeof CONSTRAINT_RENDERERS[keyof typeof CONSTRAINT_RENDERERS]>;
 export const CONSTRAINT_RENDERERS = {
     ['box']: BoxRender,
 
+    ['given']: GivenRender,
+    ['thermo']: null,
+    ['arrow']: null,
+    ['sandwich']: null,
+
     ['diagonal']: null,
     ['knight']: null,
     ['king']: null,
     ['disjointGroups']: null,
     ['consecutive']: null,
-
-    ['given']: null,
-    ['thermo']: null,
-    ['arrow']: null,
-    ['sandwich']: null,
 } as const;
+
+export function idx2rc(idx: number, { width }: { width: number }): { r: number, c: number } {
+    const r = Math.floor(idx / width);
+    const c = idx % width;
+    return { r, c };
+}
+
+export function idx2xy(idx: number, grid: { width: number }): { x: number, y: number } {
+    const { r, c } = idx2rc(idx, grid);
+    return { x: GRID_SCALE * c, y: GRID_SCALE * r };
+}
 
 boardState.update({
     grid: {
---
+++ src/svelte/board/constraint/GivenRender.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { idx2xy } from "../../../js/board";
+    import { GRID_SCALE } from "../../../js/consts";
+    import type { StateRef } from "../../../js/state_manager";
+
+    export let id: string;
+    export let ref: StateRef;
+    export let grid: { width: number, height: number };
+</script>
+
+<g {id}>
+    {#each Object.entries($ref) as [ idx, num ] (idx)}
+        <text x={idx2xy(+idx, grid).x + 0.5 * GRID_SCALE} y={idx2xy(+idx, grid).y + 0.5 * GRID_SCALE} text-anchor="middle" dominant-baseline="central" font-size={0.6 * GRID_SCALE}>{num}</text>
+    {/each}
+</g>
