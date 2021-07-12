cd44e56394112a07802f40944d1d3c7909be9c67
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-11 18:15:44 -0700

    Set givens as default (temp workaround)

--- packages/web/src/js/input.ts
+++ packages/web/src/js/input.ts
@@ -3,3 +3,6 @@ import { writable } from 'svelte/store';
 export const SELECTED_TOOL_NAME = 'selectedTool';
 export const selectedTool = writable<string>();
 (window as any).selectedTool = selectedTool;
+
+// Set "Givens" as default selected.
+selectedTool.set('10130'); // TODO: magic number.
