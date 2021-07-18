f6a857aac92d9e8e9055d85cea2697c59510b6d7
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-18 01:22:51 -0700

    Include labels in issue links

--- packages/web/src/js/github.ts
+++ packages/web/src/js/github.ts
@@ -5,7 +5,7 @@ export const SUDOKU_STUDIO_VERSION = process.env.SUDOKU_STUDIO_VERSION || 'DEV';
 
 function issueTemplateToUrl(md: string): string {
     console.log(md);
-    const title = encodeURIComponent(/^title:\s*['"](.*?)['"]?$/m.exec(md)?.[1] || '');
+    const title = encodeURIComponent(/^title:\s*['"]?(.*?)['"]?$/m.exec(md)?.[1] || '');
 
     let body = md.slice(3 + md.lastIndexOf('---'));
     body = body.replace('Sudoku Studio version:', `Sudoku Studio version: ${SUDOKU_STUDIO_VERSION}`);
@@ -13,7 +13,9 @@ function issueTemplateToUrl(md: string): string {
     body = body.replace('Operating system:', `Operating system: ${navigator.platform}`);
     body = encodeURIComponent(body);
 
-    return `https://github.com/SudokuStudio/SudokuStudio/issues/new?title=${title}&body=${body}`;
+    const labels = /^labels:\s*['"]?(.*?)['"]?$/m.exec(md)?.[1] || ''
+
+    return `https://github.com/SudokuStudio/SudokuStudio/issues/new?title=${title}&body=${body}&labels=${labels}`;
 }
 
 export const URL_BUG_REPORT = issueTemplateToUrl(bugReportMd);
