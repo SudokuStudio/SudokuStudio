465fa9c2a9c8d4e2216ee7f5f34dab60b2e4df74
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-25 20:43:25 -0700

    Update issue templates

--- packages/web/src/js/github.ts
+++ packages/web/src/js/github.ts
@@ -1,5 +1,5 @@
 export const URL_NEW_ISSUE = 'https://github.com/SudokuStudio/SudokuStudio/issues/new';
 export const URL_REPORT_BUG = URL_NEW_ISSUE
-    + '?labels=bug&template=bug_report.yml&title=%F0%9F%90%9E+Bug%3A+';
+    + '?labels=%F0%9F%90%9E%20bug&template=bug_report.yml&title=%F0%9F%90%9E+Bug%3A+';
 export const URL_REQUEST_FEATURE = URL_NEW_ISSUE
-    + '?labels=enhancement&template=feature_request.yml&title=%E2%9C%A8+Feature%3A+';
+    + '?labels=%E2%9C%A8%20enhancement&template=feature_request.yml&title=%E2%9C%A8+Feature%3A+';
