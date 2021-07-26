d4d32cc843da3088e7a6c75477cad0c5c6dbb54c
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-26 16:15:55 -0700

    Add warnings for thermo & slowThermo

--- packages/web/src/js/element/lines.ts
+++ packages/web/src/js/element/lines.ts
@@ -1,8 +1,9 @@
-import type { Grid } from "@sudoku-studio/schema";
+import type { Geometry, Grid, IdxBitset, IdxMap, schema } from "@sudoku-studio/schema";
 import type { StateRef } from "@sudoku-studio/state-manager";
 import type { InputHandler } from "../input/inputHandler";
 import type { ElementInfo } from "./element";
 import { getLineInputHandler } from "../input/lineInputHandler";
+import { arrayObj2array } from "@sudoku-studio/board-utils";
 
 export const thermoInfo: ElementInfo = {
     getInputHandler(ref: StateRef, grid: Grid, svg: SVGSVGElement): InputHandler {
@@ -18,6 +19,9 @@ export const thermoInfo: ElementInfo = {
         type: 'select',
         name: 'Thermo',
         icon: 'thermo',
+    },
+    getWarnings(value: schema.LineElement['value'], _grid: Grid, digits: IdxMap<Geometry.CELL, number>, warnings: IdxBitset<Geometry.CELL>): void {
+        getThermoWarnings(value, digits, warnings, true);
     },
 };
 
@@ -36,7 +40,57 @@ export const slowThermoInfo: ElementInfo = {
         name: 'Slow Thermo',
         icon: 'slow-thermo',
     },
+    getWarnings(value: schema.LineElement['value'], _grid: Grid, digits: IdxMap<Geometry.CELL, number>, warnings: IdxBitset<Geometry.CELL>): void {
+        getThermoWarnings(value, digits, warnings, false);
+    },
 };
+
+function getThermoWarnings(
+    value: schema.LineElement['value'],
+    digits: IdxMap<Geometry.CELL, number>, warnings: IdxBitset<Geometry.CELL>,
+    strict: boolean): void
+{
+    for (const cells of Object.values(value || {})) {
+        const cellsArr = arrayObj2array(cells);
+
+        {
+            let maxSeen = Number.NEGATIVE_INFINITY;
+            for (const cellIdx of cellsArr) {
+                const digit = digits[cellIdx];
+                if (null == digit) continue;
+
+                if (maxSeen < digit) {
+                    maxSeen = digit;
+                }
+                else if (maxSeen == digit) {
+                    warnings[cellIdx] = strict;
+                }
+                else { // maxSeen > digit.
+                    warnings[cellIdx] = true;
+                }
+            }
+        }
+
+        {
+            cellsArr.reverse();
+            let minSeen = Number.POSITIVE_INFINITY;
+            for (const cellIdx of cellsArr) {
+                const digit = digits[cellIdx];
+                if (null == digit) continue;
+
+                if (minSeen > digit) {
+                    minSeen = digit;
+                }
+                else if (minSeen == digit) {
+                    warnings[cellIdx] = strict;
+                }
+                else { // maxSeen < digit.
+                    warnings[cellIdx] = true;
+                }
+            }
+        }
+    }
+}
 
 export const betweenInfo: ElementInfo = {
     getInputHandler(ref: StateRef, grid: Grid, svg: SVGSVGElement): InputHandler {
