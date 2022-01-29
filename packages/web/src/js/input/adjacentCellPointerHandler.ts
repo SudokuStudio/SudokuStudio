d404789fc59d77be5cec9d92e5f0d53936659c1e
wooferzfg <spapushin@gmail.com>
2022-01-29 06:10:22 -0600

    Update how AdjacentCellPointerHandler triggers events

--- packages/web/src/js/input/adjacentCellPointerHandler.ts
+++ packages/web/src/js/input/adjacentCellPointerHandler.ts
@@ -10,7 +10,7 @@ export type CellDragTapEvent = {
 export class AdjacentCellPointerHandler {
     onTap: null | ((event: CellDragTapEvent) => void) = null;
     onDrag: null | ((event: CellDragTapEvent) => void) = null;
-    onDragStart: null | ((event: MouseEvent) => void) = null;
+    onDragStart: null | ((event: CellDragTapEvent) => void) = null;
     onDragEnd: null | (() => void) = null;
 
     private readonly _interpolateOnReender: boolean;
@@ -24,10 +24,10 @@ export class AdjacentCellPointerHandler {
         this._interpolateOnReender = interpolateOnReenter;
     }
 
-    down(event: MouseEvent): void {
-        this.onDragStart && this.onDragStart(event);
+    down(event: MouseEvent, grid: Grid, svg: SVGSVGElement): void {
         this._isDown = true;
         this._isTap = true;
+        this._handle(event, grid, svg);
     }
 
     move(event: MouseEvent, grid: Grid, svg: SVGSVGElement) {
@@ -78,14 +78,27 @@ export class AdjacentCellPointerHandler {
             const isFirstClick = null == this._prevPos;
             const coord = svgCoord2cellCoord(pos, grid, !isFirstClick);
             if (null != coord) {
-                if (this._prevCell !== cellCoord2CellIdx(coord, grid)) {
-                    if (null != this._prevCell)
-                        this._isTap = false; // Cancel tap as soon as moved one cell.
-
-                    this.onDrag && this.onDrag({ event, coord, grid });
-                    this._prevCell = cellCoord2CellIdx(coord, grid);
-                }
                 this._prevPos = pos;
+                const currentCellIndex = cellCoord2CellIdx(coord, grid);
+
+                if (null != this._prevCell) {
+                    // Ignore a drag within the same cell as the previous cell
+                    if (this._prevCell === currentCellIndex) {
+                        return;
+                    }
+
+                    // Cancel any active tap since we have moved between cells
+                    this._isTap = false;
+
+                    // Trigger drag event for the cell where the mouse is
+                    this.onDrag && this.onDrag({ event, coord, grid });
+                } else if (this._isTap) {
+                    // Trigger drag start event at start of click
+                    this.onDragStart && this.onDragStart({ event, coord, grid });
+                }
+
+                // Update previous cell to the cell where the mouse is
+                this._prevCell = currentCellIndex;
             }
             // Otherwise otherwise reset prevPos if pointer's off the grid.
             else if (!(this._interpolateOnReender || isOnGrid(pos, grid))) {
