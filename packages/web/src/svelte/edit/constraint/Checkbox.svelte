6b41fb297d2e15d69215a222e615071f18e77440
wooferzfg <spapushin@gmail.com>
2022-01-31 22:39:56 -0500

    Update checkboxes to work with undo/redo

--- packages/web/src/svelte/edit/constraint/Checkbox.svelte
+++ packages/web/src/svelte/edit/constraint/Checkbox.svelte
@@ -2,11 +2,17 @@
     let counter = 0;
 </script>
 <script lang="ts">
+    import { pushHistory } from "../../../js/history";
     import type { StateRef } from '@sudoku-studio/state-manager';
 
     export let name: string;
     export let icon: string;
     export let checked: StateRef;
+
+    function onClick() {
+        const diff = checked.replace(!checked.get<boolean>());
+        pushHistory(diff);
+    }
 </script>
 
 <div class="constraint-checkbox" on:click|stopPropagation>
@@ -14,7 +20,7 @@
         <span class="icon icon-inline icon-c-text icon-{icon}" />
         <span class="sr-only">{name}</span>
     </label>
-    <input id="checkbox-{counter}" type="checkbox" bind:checked={$checked} />
+    <input id="checkbox-{counter}" type="checkbox" on:change={onClick} checked={$checked} />
 </div>
 
 <style lang="scss">
