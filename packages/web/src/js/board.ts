78934f425e844b87661e1a567cd6f48219fedf3f
wooferzfg <spapushin@gmail.com>
2022-03-02 22:38:08 -0500

    Show true candidates as pencil marks

--- packages/web/src/js/board.ts
+++ packages/web/src/js/board.ts
@@ -9,19 +9,43 @@ export const boardDiv = writable<HTMLDivElement>();
 export const boardState = (window as any).boardState = new StateManager();
 export const boardGridRef = boardState.ref('grid');
 
+function getElements(): Record<string, schema.Element> | null {
+    return boardState.get<schema.Board['elements']>('elements');
+}
+
+function getElementKey(elements: Record<string, schema.Element>, markType: string) {
+    return Object.keys(elements!).find((elementKey) => markType === elements![elementKey].type);
+}
+
+export function getTypeForElementKey(elementKey: string) {
+    return getElements()![elementKey].type;
+}
+
 export function getDigits(includeGivens: boolean = true, includeFilled: boolean = true): IdxMap<Geometry.CELL, number> {
-    return getDigitsHelper(boardState.get<schema.Board['elements']>('elements') || {}, includeGivens, includeFilled);
+    return getDigitsHelper(getElements() || {}, includeGivens, includeFilled);
 }
 
 export function getCellValue(markType: string, cellIndex: Idx<Geometry.CELL>): Data {
-    const elements = boardState.get<schema.Board['elements']>('elements');
-    const elementKey = Object.keys(elements!).find((elementKey) => markType === elements![elementKey].type);
+    const elements = getElements();
+    const elementKey = getElementKey(elements!, markType);
 
     if (null == elementKey) {
         return null;
     }
 
     return boardState.get('elements', elementKey, 'value', `${cellIndex}`);
+}
+
+export function setCellValue(markType: string, cellIndex: Idx<Geometry.CELL>, newValue: Data): void {
+    const elements = getElements();
+    const elementKey = getElementKey(elements!, markType);
+
+    if (null == elementKey) {
+        throw Error(`Mark type ${markType} not found for cell ${cellIndex}`);
+    }
+
+    const elementRef = boardState.ref('elements', elementKey, 'value', `${cellIndex}`);
+    elementRef.replace(newValue);
 }
 
 export const warningState = (window as any).warningState = new StateManager();
