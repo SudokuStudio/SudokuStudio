cb26c3130190374d06eb26456e3407fbd122d3af
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-05-12 12:42:35 -0700

    test: integration test for basic selection/input

--- packages/web/tests/screenshot.spec.ts
+++ packages/web/tests/screenshot.spec.ts
@@ -4,7 +4,7 @@ import { fPuzzlesBoards, sudokuStudioBoards, sudokuStudioCoverageBoards } from '
 
 test('full page', async ({ page }) => {
     const [_name, board, _solution] = sudokuStudioCoverageBoards[0];
-    await page.goto(`?b=${board}`);
+    await page.goto(`/?b=${board}`);
     await page.waitForSelector('svg');
     await expect(page).toHaveScreenshot();
 });
@@ -14,7 +14,7 @@ test.describe('board', () => {
         for (const [name, board, _solution] of sudokuStudioBoards) {
             test(name, async ({ page }) => {
                 page.setViewportSize({ width: 800, height: 800 });
-                await page.goto(`?b=${board}`);
+                await page.goto(`/?b=${board}`);
                 await expect(page.locator('svg')).toHaveScreenshot();
             });
         }
@@ -24,7 +24,7 @@ test.describe('board', () => {
         for (const [name, board, _solution] of fPuzzlesBoards) {
             test(name, async ({ page }) => {
                 page.setViewportSize({ width: 800, height: 800 });
-                await page.goto(`?f=${board}`);
+                await page.goto(`/?f=${board}`);
                 await page.waitForURL(/b=/);
                 await expect(page.locator('svg')).toHaveScreenshot();
             });
@@ -35,7 +35,7 @@ test.describe('board', () => {
 test.describe('save image', () => {
     for (const [name, board, _solution] of sudokuStudioCoverageBoards) {
         test(name, async ({ page }) => {
-            await page.goto(`?b=${board}`);
+            await page.goto(`/?b=${board}`);
             const downloadPromise = page.waitForEvent('download');
             const saveImageButton = page.getByRole('button', { name: 'Save Image' });
             await saveImageButton.click();
@@ -43,4 +43,32 @@ test.describe('save image', () => {
             expect(await buffer(await download.createReadStream())).toMatchSnapshot();
         });
     }
+});
+
+test('selection/input', async ({ page }) => {
+    page.setViewportSize({ width: 800, height: 800 });
+    await page.goto('/');
+    await page.waitForSelector('svg');
+    const overlay = page.locator('div.overlay');
+    const bbox = (await overlay.boundingBox())!;
+    await page.mouse.move(bbox.x + bbox.width * (1 / 18), bbox.y + bbox.height * (1 / 18)); // 1,1
+    await page.mouse.down();
+    await page.mouse.move(bbox.x + bbox.width * (3 / 18), bbox.y + bbox.height * (3 / 18)); // 2,2
+    await page.mouse.move(bbox.x + bbox.width * (3 / 18), bbox.y + bbox.height * (5 / 18)); // 2,3
+    await page.mouse.move(bbox.x + bbox.width * (5 / 18), bbox.y + bbox.height * (5 / 18)); // 3,3
+    await page.mouse.up();
+    await expect(page.locator('svg')).toHaveScreenshot();
+
+    await page.keyboard.press('Shift+1');
+    await page.keyboard.press('Shift+2');
+    await page.keyboard.press('Shift+3');
+    await page.keyboard.press('ControlOrMeta+3');
+    await page.keyboard.press('ControlOrMeta+4');
+    await page.keyboard.press('ControlOrMeta+5');
+    await expect(page.locator('svg')).toHaveScreenshot();
+
+    await page.keyboard.press('6');
+    await page.mouse.click(bbox.x + bbox.width * (1 / 18), bbox.y + bbox.height * (1 / 18)); // 1,1
+    await page.keyboard.press('7');
+    await expect(page.locator('svg')).toHaveScreenshot();
 });
