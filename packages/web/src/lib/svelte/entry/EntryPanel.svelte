4428a62115bcd7de2ce79f9b5cebe9a9c6fc0314
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-06-06 18:28:36 -0700

    fixup! fixup! begin work on separate play page

--- packages/web/src/lib/svelte/entry/EntryPanel.svelte
+++ packages/web/src/lib/svelte/entry/EntryPanel.svelte
@@ -2,20 +2,10 @@
     import { onDestroy } from 'svelte';
     import ButtonPad from './ButtonPad.svelte';
     import EntryInfo from './EntryInfo.svelte';
-
-    // Unfortunately there is no way to size the text to based on the parent (in this case button) size in native CSS.
-    // So we have to use a bit of javascript to set the font size when the page resizes.
-    let entryPadEl: HTMLDivElement;
-    function onResize() {
-        entryPadEl.style.fontSize = `${0.12 * entryPadEl.clientWidth}px`;
-    }
-    window.addEventListener('resize', onResize);
-    onDestroy(() => window.removeEventListener('resize', onResize));
-    requestAnimationFrame(onResize); // Run on load.
 </script>
 
 <div class="entry-column">
-    <div class="entry-pad" bind:this={entryPadEl}>
+    <div class="entry-pad">
         <ButtonPad />
     </div>
     <EntryInfo />
